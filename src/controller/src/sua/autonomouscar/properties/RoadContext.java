b0ec582ab6af5e709412773f864a60f83d0fa498
Adriano Vega <adrianovega@hotmail.com>
2021-04-25 00:03:34 +0200

    Corregir el registro de las propiedades

--- src/controller/src/sua/autonomouscar/properties/RoadContext.java
+++ src/controller/src/sua/autonomouscar/properties/RoadContext.java
@@ -1,5 +1,8 @@
 package sua.autonomouscar.properties;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import sua.autonomouscar.interfaces.ERoadStatus;
 import sua.autonomouscar.interfaces.ERoadType;
 
@@ -7,28 +10,37 @@ import sua.autonomouscar.interfaces.ERoadType;
  * The adaption property that represents the road context. Includes the values of the status and type.
  */
 public class RoadContext {
-	private ERoadStatus status;
-	private ERoadType type;
+	private static final String STATUS = "status";
+
+	private static final String TYPE = "type";
+
+	private Dictionary<String, Object> properties;
 
 	public RoadContext(ERoadStatus status, ERoadType type)
 	{
-		this.status = status;
-		this.type = type;
+		this.properties = new Hashtable<>();
+
+		this.setStatus(status);
+		this.setType(type);
 	}
 
 	public ERoadType getType() {
-		return type;
+		return (ERoadType)properties.get(TYPE);
 	}
 
 	public ERoadStatus getStatus() {
-		return status;
+		return (ERoadStatus)properties.get(STATUS);
 	}
 
 	public void setType(ERoadType type) {
-		this.type = type;
+		this.properties.put(TYPE, type);
 	}
 
 	public void setStatus(ERoadStatus status) {
-		this.status = status;
+		this.properties.put(STATUS, status);
+	}
+
+	public Dictionary<String, Object> getProperties() {
+		return properties;
 	}
 }
