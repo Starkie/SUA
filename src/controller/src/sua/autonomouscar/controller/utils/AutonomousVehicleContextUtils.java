7e40e7f803cc0891d7ba36878c5bc1f0c8e70245
Adriano Vega <adrianovega@hotmail.com>
2021-04-25 19:16:07 +0200

    Implementar una función para buscar el mejor sensor de distancia disponible

--- src/controller/src/sua/autonomouscar/controller/utils/AutonomousVehicleContextUtils.java
+++ src/controller/src/sua/autonomouscar/controller/utils/AutonomousVehicleContextUtils.java
@@ -7,7 +7,10 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 
+import sua.autonomouscar.devices.interfaces.IDistanceSensor;
 import sua.autonomouscar.driving.interfaces.IDrivingService;
+import sua.autonomouscar.infrastructure.OSGiUtils;
+import sua.autonomouscar.interfaces.IIdentifiable;
 
 /**
  * Class with utility methods to interact with the autonomous vehicle OSGi context.
@@ -45,5 +48,23 @@ public class AutonomousVehicleContextUtils {
 				.orElseGet(() -> null);
 		
 		return currentDrivingService;
+	}
+	
+	/**
+	 * Searches for the best {@link IDistanceSensor} in the given {@link DistanceSensorPositon} that is currently active in the vehicle.
+	 * @param context The autonomous vehicle OSGi context.
+	 * @param position The position of the sensor to find.
+	 * @return The best available distance sensor in the given position, if found. Otherwise, returns null.
+	 */
+	public static IDistanceSensor findDistanceSensor(BundleContext context, DistanceSensorPositon position) {
+		// Always prioritize the normal distance sensor before the LIDAR.
+		IDistanceSensor distanceSensor = OSGiUtils.getService(context, IDistanceSensor.class, String.format("(%s=%s)", IIdentifiable.ID, position.getNormalSensorId()));
+		
+		if (distanceSensor == null)
+		{
+			distanceSensor = OSGiUtils.getService(context, IDistanceSensor.class, String.format("(%s=%s)", IIdentifiable.ID, position.getLidarSensorId()));
+		}
+		
+		return distanceSensor;
 	}
 }
---
+++ src/controller/src/sua/autonomouscar/controller/utils/DistanceSensorPositon.java
@@ -0,0 +1,32 @@
+package sua.autonomouscar.controller.utils;
+
+import sua.autonomouscar.devices.interfaces.IDistanceSensor;
+
+/**
+ * Represents the possible positions of a {@link IDistanceSensor} in the vehicle.
+ */
+public enum DistanceSensorPositon {
+	FRONT("FrontDistanceSensor", "LIDAR-FrontDistanceSensor"),
+	REAR("RearDistanceSensor", "LIDAR-RearDistanceSensor"),
+	LEFT("LeftDistanceSensor", "LIDAR-LeftDistanceSensor"),
+	RIGHT("RightDistanceSensor", "LIDAR-RightDistanceSensor");
+	
+	// The identifier of the normal distance sensor in that position. 
+	private final String normalSensorId;
+	
+	// The identifier of the LIDAR distance sensor in that position.
+	private final String lidarSensorId;
+	
+	private DistanceSensorPositon(String normalSensorId, String lidarSensorId) {
+		this.normalSensorId = lidarSensorId;
+		this.lidarSensorId = normalSensorId;
+	}
+
+	public String getNormalSensorId() {
+		return normalSensorId;
+	}
+
+	public String getLidarSensorId() {
+		return lidarSensorId;
+	} 
+}
