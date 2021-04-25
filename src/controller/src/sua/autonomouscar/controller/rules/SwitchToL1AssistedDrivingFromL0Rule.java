61fa8dda9a23f4cab3660f01e8dc20d8034b1b07
Adriano Vega <adrianovega@hotmail.com>
2021-04-25 19:16:45 +0200

    Obtener el distance sensor frontal en la regla de adaptación

--- src/controller/src/sua/autonomouscar/controller/rules/SwitchToL1AssistedDrivingFromL0Rule.java
+++ src/controller/src/sua/autonomouscar/controller/rules/SwitchToL1AssistedDrivingFromL0Rule.java
@@ -5,9 +5,14 @@ import org.osgi.framework.ServiceReference;
 
 import sua.autonomouscar.controller.interfaces.IAdaptionRule;
 import sua.autonomouscar.controller.utils.AutonomousVehicleContextUtils;
+import sua.autonomouscar.controller.utils.DistanceSensorPositon;
 import sua.autonomouscar.controller.utils.DrivingServiceUtils;
+import sua.autonomouscar.devices.interfaces.IDistanceSensor;
 import sua.autonomouscar.driving.interfaces.IDrivingService;
 import sua.autonomouscar.driving.interfaces.IL1_AssistedDriving;
+import sua.autonomouscar.infrastructure.OSGiUtils;
+import sua.autonomouscar.infrastructure.Thing;
+import sua.autonomouscar.interfaces.IIdentifiable;
 
 public class SwitchToL1AssistedDrivingFromL0Rule implements IAdaptionRule {
 	
@@ -21,7 +26,6 @@ public class SwitchToL1AssistedDrivingFromL0Rule implements IAdaptionRule {
 	public void evaluateAndExecute() {
 		IDrivingService currentDrivingService = AutonomousVehicleContextUtils.findCurrentDrivingService(context);
 		
-		// TODO: Add a condition to check that the LineSensors and the FrontDistanceSensor are available.
 		if (!DrivingServiceUtils.isL0DrivingService(currentDrivingService))
 		{
 			return; 
@@ -30,12 +34,17 @@ public class SwitchToL1AssistedDrivingFromL0Rule implements IAdaptionRule {
 		ServiceReference<IL1_AssistedDriving> l1DrivingServiceReference = context.getServiceReference(IL1_AssistedDriving.class);
 		IL1_AssistedDriving l1DrivingService = context.getService(l1DrivingServiceReference);
 		
-		if (l1DrivingService == null)
+		IDistanceSensor distanceSensor = AutonomousVehicleContextUtils.findDistanceSensor(context, DistanceSensorPositon.FRONT);
+		
+		if (l1DrivingService == null 
+			|| distanceSensor == null)
 		{
 			return;
 		}
 		
-		System.out.println("[Controller] Executing the " + this.getClass().getSimpleName() + " rule.");
+		System.out.println("[ Controller ] Executing the " + this.getClass().getSimpleName() + " rule.");
+		
+		l1DrivingService.setFrontDistanceSensor(((Thing)distanceSensor).getId());
 		
 		currentDrivingService.stopDriving();
 		l1DrivingService.startDriving();
