649100a3aedfd0133847d084f007fcb736c5bf36
Adriano Vega <adrianovega@hotmail.com>
2021-04-25 19:17:16 +0200

    Registrar la regla de adaptación en el configurador del simulador

--- src/AutonomousCar.simulation.console.commands/src/sua/autonomouscar/simulation/console/commands/SimulationContollerConfigurator.java
+++ src/AutonomousCar.simulation.console.commands/src/sua/autonomouscar/simulation/console/commands/SimulationContollerConfigurator.java
@@ -7,6 +7,7 @@ import sua.autonomouscar.controller.SimulationContoller;
 import sua.autonomouscar.controller.monitors.road.RoadContextMonitor;
 import sua.autonomouscar.controller.probes.road.RoadStatusProbe;
 import sua.autonomouscar.controller.probes.road.RoadTypeProbe;
+import sua.autonomouscar.controller.rules.SwitchToL1AssistedDrivingFromL0Rule;
 import sua.autonomouscar.infrastructure.OSGiUtils;
 
 public class SimulationContollerConfigurator {
@@ -22,5 +23,8 @@ public class SimulationContollerConfigurator {
 		
 		// Monitors.
 		controller.addMonitor(new RoadContextMonitor(context, roadStatusProbe, roadTypeProbe));
+		
+		// Adaption rules.
+		controller.addAdaptionRule(new SwitchToL1AssistedDrivingFromL0Rule(context));
 	}
 }
