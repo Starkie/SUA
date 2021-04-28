0330c8bcce5e5c6fff05b9ea6311ff7426f71c20
Adriano Vega <adrianovega@hotmail.com>
2021-04-28 22:55:26 +0200

    Refactorizar una regla como un listener

---
+++ src/controller.rules/src/sua/autonomouscar/controller/rules/Activator.java
@@ -0,0 +1,38 @@
+package sua.autonomouscar.controller.rules;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import sua.autonomouscar.controller.properties.RoadContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+    private SwithToL2AdaptiveCruiseControlFromL1Rule swithToL2AdaptiveCruiseControlFromL1Rule;
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		
+		// Register the rule services.
+        this.swithToL2AdaptiveCruiseControlFromL1Rule = new SwithToL2AdaptiveCruiseControlFromL1Rule(context);
+        context.registerService(SwithToL2AdaptiveCruiseControlFromL1Rule.class, swithToL2AdaptiveCruiseControlFromL1Rule, null);
+
+        // Register the rule listeners to the properties it depends on.
+        String roadContextKnowledgePropertyListenerFilter= "(objectclass=" + RoadContext.class.getName() + ")";
+        context.addServiceListener(swithToL2AdaptiveCruiseControlFromL1Rule, roadContextKnowledgePropertyListenerFilter);		
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+	    context.removeServiceListener(swithToL2AdaptiveCruiseControlFromL1Rule);
+	    
+	    this.swithToL2AdaptiveCruiseControlFromL1Rule = null;
+	    
+		Activator.context = null;
+	}
+
+}
--- src/controller.rules/src/sua/autonomouscar/controller/rules/SwithToL2AdaptiveCruiseControlFromL1Rule.java
+++ src/controller.rules/src/sua/autonomouscar/controller/rules/SwithToL2AdaptiveCruiseControlFromL1Rule.java
@@ -3,7 +3,7 @@ package sua.autonomouscar.controller.rules;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
-import sua.autonomouscar.controller.interfaces.IAdaptionRule;
+import sua.autonomouscar.controller.properties.RoadContext;
 import sua.autonomouscar.controller.utils.AutonomousVehicleContextUtils;
 import sua.autonomouscar.controller.utils.DistanceSensorPositon;
 import sua.autonomouscar.controller.utils.DrivingServiceUtils;
@@ -16,15 +16,17 @@ import sua.autonomouscar.driving.interfaces.IL2_AdaptiveCruiseControl;
 import sua.autonomouscar.infrastructure.OSGiUtils;
 import sua.autonomouscar.infrastructure.Thing;
 import sua.autonomouscar.interfaces.ERoadType;
-import sua.autonomouscar.properties.RoadContext;
 
-public class SwithToL2AdaptiveCrusieControlFromL1 implements IAdaptionRule {
+public class SwithToL2AdaptiveCruiseControlFromL1Rule extends AdaptionRuleBase {
+    // The default lateral security distance is of 2.5m (250 cm).
+    private static final int LATERAL_SECURITY_DISTANCE = 250;
+    
     private BundleContext context;
 
-    public SwithToL2AdaptiveCrusieControlFromL1(BundleContext context) {
+    public SwithToL2AdaptiveCruiseControlFromL1Rule(BundleContext context) {
         this.context = context;
     }
-
+    
     @Override
     public void evaluateAndExecute() {
         IDrivingService currentDrivingService = AutonomousVehicleContextUtils.findCurrentDrivingService(context);
@@ -37,6 +39,7 @@ public class SwithToL2AdaptiveCrusieControlFromL1 implements IAdaptionRule {
                 .getServiceReference(IL2_AdaptiveCruiseControl.class);
         IL2_AdaptiveCruiseControl l2DrivingService = context.getService(l2DrivingServiceReference);
 
+        // TODO: How to disable sensors?
         ILineSensor leftLineSensor = AutonomousVehicleContextUtils.findLineSensor(context, LineSensorPosition.LEFT);
         ILineSensor rightLineSensor = AutonomousVehicleContextUtils.findLineSensor(context, LineSensorPosition.RIGHT);
 
@@ -48,7 +51,9 @@ public class SwithToL2AdaptiveCrusieControlFromL1 implements IAdaptionRule {
         RoadContext roadContext = OSGiUtils.getService(context, RoadContext.class);
         ERoadType currentRoadType = roadContext.getType();
 
-        if (l2DrivingService == null || engine == null || distanceSensor == null
+        if (l2DrivingService == null 
+                || engine == null 
+                || distanceSensor == null
                 || !(currentRoadType == ERoadType.HIGHWAY)) {
             return;
         }
@@ -57,6 +62,8 @@ public class SwithToL2AdaptiveCrusieControlFromL1 implements IAdaptionRule {
 
         l2DrivingService.setEngine(((Thing) engine).getId());
         l2DrivingService.setFrontDistanceSensor(((Thing) distanceSensor).getId());
+
+        l2DrivingService.setLateralSecurityDistance(LATERAL_SECURITY_DISTANCE);
 
         currentDrivingService.stopDriving();
         l2DrivingService.startDriving();
