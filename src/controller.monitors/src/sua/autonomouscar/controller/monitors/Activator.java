616461034debc53a4d7e61db4da17d0e200ad1e3
Adriano Vega <adrianovega@hotmail.com>
2021-05-03 00:03:06 +0200

    Añadir registro de service listeners faltante

--- src/controller.monitors/src/sua/autonomouscar/controller/monitors/Activator.java
+++ src/controller.monitors/src/sua/autonomouscar/controller/monitors/Activator.java
@@ -6,6 +6,7 @@ import org.osgi.framework.ServiceRegistration;
 
 import sua.autonomouscar.controller.monitors.car.DrivingServiceMonitor;
 import sua.autonomouscar.controller.monitors.car.EngineHealthMonitor;
+import sua.autonomouscar.controller.monitors.car.NotificationServiceHealthMonitor;
 import sua.autonomouscar.controller.monitors.road.IRoadContextMonitor;
 import sua.autonomouscar.controller.monitors.road.RoadContextMonitor;
 
@@ -13,9 +14,10 @@ public class Activator implements BundleActivator {
 
 	private static BundleContext context;
 
-	private ServiceRegistration<IRoadContextMonitor> roadContextServiceRegistration;
     private ServiceRegistration<DrivingServiceMonitor> drivingServiceMonitorRegistration;
     private ServiceRegistration<EngineHealthMonitor> engineHealthMonitor;
+    private ServiceRegistration<IRoadContextMonitor> roadContextServiceRegistration;
+    private ServiceRegistration<NotificationServiceHealthMonitor> notificationServiceHealthMonitorRegistration;
 
 	static BundleContext getContext() {
 		return context;
@@ -24,26 +26,32 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 
-		IRoadContextMonitor roadContextMonitor = new RoadContextMonitor(bundleContext);
-		this.roadContextServiceRegistration = context.registerService(IRoadContextMonitor.class, roadContextMonitor, null);
-		
 		DrivingServiceMonitor drivingServiceMonitor = new DrivingServiceMonitor(bundleContext);
         this.drivingServiceMonitorRegistration = context.registerService(DrivingServiceMonitor.class, drivingServiceMonitor, null);
-        
+
         EngineHealthMonitor engineHealthMonitor = new EngineHealthMonitor(bundleContext);
         this.engineHealthMonitor = context.registerService(EngineHealthMonitor.class, engineHealthMonitor, null);
+
+        NotificationServiceHealthMonitor notificationServiceHealthMonitor = new NotificationServiceHealthMonitor(bundleContext);
+        this.notificationServiceHealthMonitorRegistration = context.registerService(NotificationServiceHealthMonitor.class, notificationServiceHealthMonitor, null);
+
+        IRoadContextMonitor roadContextMonitor = new RoadContextMonitor(bundleContext);
+        this.roadContextServiceRegistration = context.registerService(IRoadContextMonitor.class, roadContextMonitor, null);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		this.roadContextServiceRegistration.unregister();
-		this.roadContextServiceRegistration = null;
-		
 		this.drivingServiceMonitorRegistration.unregister();
 		this.drivingServiceMonitorRegistration = null;
-		
+
 		this.engineHealthMonitor.unregister();
 		this.engineHealthMonitor = null;
-		
+
+		this.notificationServiceHealthMonitorRegistration.unregister();
+		this.notificationServiceHealthMonitorRegistration = null;
+
+		this.roadContextServiceRegistration.unregister();
+		this.roadContextServiceRegistration = null;
+
 		Activator.context = null;
 	}
 }
