1e583990c9b9dad0bc80ed74f0aff3f618baadad
Adriano Vega <adrianovega@hotmail.com>
2021-05-08 23:41:27 +0200

    Añadir un método updateProperty que permite retrasar la notificación a los listeners de un cambio en una propiedad

--- src/controller/src/sua/autonomouscar/controller/properties/KnowledgeBase.java
+++ src/controller/src/sua/autonomouscar/controller/properties/KnowledgeBase.java
@@ -26,7 +26,7 @@ public abstract class KnowledgeBase implements IKnowledge {
     /**
      * Creates a new instance of the class {@link RoadContext}. It also registers
      * the property.
-     * 
+     *
      * @param context The context to register the property.
      * @param status  The initial road status.
      * @param type    The initial road type.
@@ -35,7 +35,7 @@ public abstract class KnowledgeBase implements IKnowledge {
         this.context = context;
 
         this.properties = new Hashtable<>();
-        
+
         this.implementedInterfaces = new ArrayList<String>();
         this.implementedInterfaces.add(KnowledgeBase.class.getName());
         this.implementedInterfaces.add(IKnowledge.class.getName());
@@ -43,22 +43,38 @@ public abstract class KnowledgeBase implements IKnowledge {
 
     public ServiceRegistration<?> registerKnowledge() {
         this.serviceRegistration = this.context.registerService(implementedInterfaces.toArray(new String[0]), this, this.properties);
-        
+
         return this.serviceRegistration;
     }
 
     /**
      * Updates the properties on the {@link ServiceRegistration} of the current
      * service.
-     * 
+     *
      * @param propertyName The name of the property to update.
      * @param value        The new value of the property.
      */
     protected void updateProperty(String propertyName, Object value) {
-        this.properties.put(propertyName, value);
-        this.serviceRegistration.setProperties(properties);
+        this.updateProperty(propertyName, value, true);
     }
-    
+
+    /**
+     * Updates the properties on the {@link ServiceRegistration} of the current
+     * service.
+     *
+     * @param propertyName The name of the property to update.
+     * @param value        The new value of the property.
+     * @param notifyListeners Value indicating whether the listeners should be notified of this change. Useful for when more changes to the properties must be done, and the listeners should not be notified yet.
+     */
+    protected void updateProperty(String propertyName, Object value, boolean notifyListeners) {
+        this.properties.put(propertyName, value);
+
+        if (notifyListeners)
+        {
+            this.serviceRegistration.setProperties(properties);
+        }
+    }
+
     protected void addImplementedInterface(String interfaceName) {
         this.implementedInterfaces.add(interfaceName);
     }
