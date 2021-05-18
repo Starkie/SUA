f63d492967361aa3c99ed11fa0e47ea592104fe5
Adriano Vega <adrianovega@hotmail.com>
2021-05-18 19:24:31 +0200

    Añadir una cláusula para que comprueba que no va a cambiar a L3

--- src/controller.rules/src/sua/autonomouscar/controller/rules/autonomy/L2/SwitchToL2AdaptiveCruiseControlFromL2Rule.java
+++ src/controller.rules/src/sua/autonomouscar/controller/rules/autonomy/L2/SwitchToL2AdaptiveCruiseControlFromL2Rule.java
@@ -6,6 +6,7 @@ import sua.autonomouscar.controller.properties.car.CurrentDrivingServiceStatus;
 import sua.autonomouscar.controller.properties.car.DistanceSensorHealthStatus;
 import sua.autonomouscar.controller.properties.car.EngineHealthStatus;
 import sua.autonomouscar.controller.properties.road.RoadContext;
+import sua.autonomouscar.controller.rules.utils.L3ConfigurationUtils;
 import sua.autonomouscar.controller.utils.DrivingAutonomyLevel;
 import sua.autonomouscar.driving.interfaces.IL2_AdaptiveCruiseControl;
 import sua.autonomouscar.driving.interfaces.IL2_DrivingService;
@@ -24,7 +25,8 @@ public class SwitchToL2AdaptiveCruiseControlFromL2Rule extends SwitchToL2Adaptiv
     protected boolean evaluateRuleCondition(CurrentDrivingServiceStatus currentDrivingServiceStatus, RoadContext roadContext, DistanceSensorHealthStatus frontDistanceSensorHealthStatus, EngineHealthStatus engineHealthStatus) {
         // All the L2 required services must be available.
         boolean areAllServicesAvailable = frontDistanceSensorHealthStatus.isAvailable()
-            && engineHealthStatus.isAvailable();
+            && engineHealthStatus.isAvailable()
+            && !L3ConfigurationUtils.areAllL3RequiredServicesAvailable(this.context);
 
         // The current autonomy level must be L2, but not of the type IL2_AdaptiveCruiseControl.
         DrivingAutonomyLevel autonomyLevel = currentDrivingServiceStatus.getAutonomyLevel();
